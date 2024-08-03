3391098bc1b64bd7cd5ec0a9c78e7c2ef86c316c
Mallikarjun Naik <109130928+MallikarjunNaik@users.noreply.github.com>
2024-08-03 21:35:57 +0530

    Update 73. Set Matrix Zeroes.cpp

--- day1/73. Set Matrix Zeroes.cpp
+++ day1/73. Set Matrix Zeroes.cpp
@@ -1,3 +1,20 @@
+/*Intuition
+First, identify all zero elements and record their row and column indices. Then, use these indices to set the corresponding rows and columns to zero in the matrix.
+
+Approach
+The approach for setting matrix rows and columns to zero when a zero is found can be summarized in the following steps:
+
+Identify the Positions of Zeros:
+
+Iterate through the matrix and record the positions (row and column indices) of all zero elements. Store these positions in two separate lists: one for rows and one for columns.
+Set Entire Columns to Zero:
+
+For each column index stored in the column list, iterate through each row of that column and set the elements to zero.
+Set Entire Rows to Zero:
+
+For each row index stored in the row list, iterate through each column of that row and set the elements to zero.
+    */
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
