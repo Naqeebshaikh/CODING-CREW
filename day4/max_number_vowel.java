e7887d1bbbf4ef55f7d457a2fdd17237fd891e1b
Akash Desai <137322203+Akash0o7@users.noreply.github.com>
2024-08-06 18:52:38 +0530

    Create max_number_vowel.java

---
+++ day4/max_number_vowel.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxVowels(String s, int k) {
+        int maxVowels = 0;
+        int currentVowels = 0;
+        int n = s.length();
+        for (int i = 0; i < k; i++) {
+            if (isVowel(s.charAt(i))) {
+                currentVowels++;
+            }
+        }
+        maxVowels = currentVowels;
+
+        for (int i = k; i < n; i++) {
+            if (isVowel(s.charAt(i))) {
+                currentVowels++;
+            }
+            if (isVowel(s.charAt(i - k))) {
+                currentVowels--;
+            }
+            maxVowels = Math.max(maxVowels, currentVowels);
+        }
+
+        return maxVowels;
+    }
+
+    public static boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+
+}
